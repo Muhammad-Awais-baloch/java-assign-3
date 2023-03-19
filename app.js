7eaf626a354aafe741a7da6711aae9f8eb1f6169
AwaiaJogi <m.awais67561@gmail.com>
2023-03-19 12:45:03 -0700

    java Assign 3_web

---
+++ app.js
@@ -0,0 +1,174 @@
+//chapter21 to 25
+//Question1
+// var userName1 = prompt("Enter your first Name")
+// var userName2 = prompt("Enter your Last Name")
+// var margName = userName1+" "+userName2
+// document.write(margName)
+
+
+
+
+
+//Question2
+// var Phone = prompt("Enter Your Favourite Phone Model")
+// var Result = Phone.length
+// document.write("My favourite phone is:" +""+ Phone +"<br/>"+ "Length of String:" +""+ Result)
+
+
+
+
+
+//Question3
+// var country = "Pakistani"
+// var Result = country.indexOf("n")
+// document.write("String:"+" "+"Pakistani"+"<br/>"+"index of 'n'"+" "+"="+" "+Result)
+
+
+
+//Question4
+// var str = "Hello World"
+// var Result = str.lastIndexOf("l")
+// document.write("String:"+" "+str+"<br/>"+"Last index of 'l'"+" "+"="+" "+Result)
+
+
+
+//Question5
+// var country = "Pakistani"
+// var Result = country.charAt(3)
+// document.write("String:"+" "+"Pakistani"+"<br/>"+"index of 3"+" "+"="+" "+Result)
+
+
+
+//Question6
+// var firstName = prompt("Enter your first Name")
+// var lastName = prompt("Enter your Last Name")
+// var Result = firstName+lastName
+// var concatName = Result.concat()
+// document.write(concatName)
+
+
+
+//Question7
+// var string = "Hyderabad"
+// var Result = string.replace("Hyder", "Islam")
+// document.write("City:"+string+"<br/>"+"After Replacement:"+Result)
+
+
+
+
+//Question8
+// var message = "Ali and Sami are best friends.They play cricket and football together."
+// var Result = message.replace(/and/g, "&" )
+// document.write("Message:"+message+"<br/>"+"After Replacement:"+" "+Result)
+
+
+
+
+//Question9
+// var string  = "472"
+// var result = Number(string)
+// document.write(result)
+
+
+
+
+//Question10
+// var country = prompt("Enter Your Input")
+// var result = country.toUpperCase()
+// document.write(result)
+
+
+
+//chapter 26 to 30
+
+
+
+//Question19
+// var Num = 3.45214
+//  var result = Math.round(Num)
+//  document.write(result)
+
+
+
+// var Num = 3.45214
+// var result = Math.ceil(Num)
+// document.write(result)
+
+
+
+// var Num = 3.45214
+// var result = Math.floor(Num)
+// document.write(result)
+
+
+
+//Question20
+// var Num = +prompt("Enter Nagitive Num")
+// var result = Number(Num)
+// document.write(result)
+
+
+
+
+// var Num = +prompt("Enter Nagitive Num")
+// var result = Math.round(Num)
+// document.write(result)
+
+
+
+// var Num = +prompt("Enter Nagitive Num")
+// var result = Math.floor(Num)
+// document.write(result)
+
+
+
+// var Num = +prompt("Enter Nagitive Num")
+// var result = Math.ceil(Num)
+// document.write(result)
+
+
+
+
+//Question21
+// var Num = +prompt("Enter your Num")
+// var result = Math.abs(Num)
+// document.write(result)
+
+
+
+
+//chapter 31 to 34
+//Question25
+
+// var Num = +prompt("Enter Number from 1 to 10")
+// var result = Math.floor(Math.random() * 10) + 1
+// document.write("Your Number:"+Num+"<br/>")
+// document.write("Random Number:"+" "+result)
+
+// if(result===Num){
+//     document.write("<br/>"+"Congratulations!")
+// }
+// else{
+//     document.write("<br/>"+"Sorry")
+// }
+
+
+
+//Question26
+// var now = new Date()
+// document.write(now)
+
+
+
+//Question27
+// var now = new Date().getMonth()
+// document.write( now)
+
+
+
+//Question28
+//  var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+//  var day = new Date().getDay()
+//  document.write(day)
+
+
